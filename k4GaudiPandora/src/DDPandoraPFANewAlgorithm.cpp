2e378929e85f08777468b18e70a329390cf9d892
jmcarcell <jmcarcell@users.noreply.github.com>
2025-08-07 20:41:34 +0200

    Pass the GeoSvc to DDTrackCreatorCLIC

--- k4GaudiPandora/src/DDPandoraPFANewAlgorithm.cpp
+++ k4GaudiPandora/src/DDPandoraPFANewAlgorithm.cpp
@@ -129,7 +129,7 @@ StatusCode DDPandoraPFANewAlgorithm::initialize() {
 
   /// TODO: IMPLEMENT ILD
   if (m_settings.m_trackCreatorName == "DDTrackCreatorCLIC")
-    m_pTrackCreator = std::make_unique<DDTrackCreatorCLIC>(m_trackCreatorSettings, m_pPandora, this);
+    m_pTrackCreator = std::make_unique<DDTrackCreatorCLIC>(m_trackCreatorSettings, m_pPandora, this, m_geoSvc);
   // else if (m_settings.m_trackCreatorName == "DDTrackCreatorILD")
   // m_pTrackCreator = std::make_unique<DDTrackCreatorILD>(m_trackCreatorSettings, m_pPandora);
   else
